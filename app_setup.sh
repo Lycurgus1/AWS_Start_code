811e4d265614386dd26d6d66badeb82ef1b7fba9
Lycurgus1 <max.palmer.official@outlook.com>
2020-08-13 10:16:09 +0100

    Updating IPS

--- app_setup.sh
+++ app_setup.sh
@@ -1,8 +1,8 @@
 # Transferring the folders to the database server and  then calling a provision file to automatically >
 
 # copy app folder into server, using devsops student file as key
-scp -i ~/.ssh/DevOpsStudents.pem -r app/ ubuntu@3.249.133.61:~/app/
+scp -i ~/.ssh/DevOpsStudents.pem -r app/ ubuntu@52.51.223.116:~/app/
 
 # enter app machine
-ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@3.249.133.61
+ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@52.51.223.116
 
